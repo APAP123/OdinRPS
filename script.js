7ef2856bb5941a7985a42553b027c0c994b99aa0
zork <adampapirnik@gmail.com>
2021-12-07 14:49:35 -0600

    Implemented JavaScript logic and changed some ids on HTML elements into classes instead

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection) {
     switch(playerSelection) {
         case 'rock':
+            console.log("player chose rock");
             if(computerSelection === 'rock') {
                 return 0; //tie
             }
@@ -51,50 +52,96 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function playerPlay(){
-    let message = "Rock, Paper, or Scissors?";
-    while(true) {
-        let result = prompt(message);
-        console.log("result is " + result)
-        if ( (result.toLowerCase() != "rock") && (result.toLowerCase() != "paper") && (result.toLowerCase() != "scissors")) {
-            message = "Invalid response. Response must be 'rock', 'paper', or 'scissors'.";
-        }
-        else {
-            return result;
-        }
+// function playerPlay(){
+//     let message = "Rock, Paper, or Scissors?";
+//     while(true) {
+//         let result = prompt(message);
+//         console.log("result is " + result)
+//         if ( (result.toLowerCase() != "rock") && (result.toLowerCase() != "paper") && (result.toLowerCase() != "scissors")) {
+//             message = "Invalid response. Response must be 'rock', 'paper', or 'scissors'.";
+//         }
+//         else {
+//             return result;
+//         }
+//     }
+// }
+
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = playerPlay();
+//         let computerSelection = computerPlay();
+
+//         result = playRound(playerSelection, computerSelection);
+
+//         switch(result){
+//             case -1:
+//                 computerScore++;
+//                 console.log("You lose! " + computerSelection + " beats " + playerSelection + ".");
+//                 break;
+//             case 0:
+//                 console.log("It's a tie!");
+//                 break;
+//             case 1:
+//                 playerScore++;
+//                 console.log("You win! " + playerSelection + " beats " + computerSelection + ".");
+//         }
+//     }
+
+//     console.log("Final Score: Player - " + playerScore + ", Computer - " + computerScore);
+// }
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+const buttons = document.querySelectorAll('button');
+
+const score = document.querySelector('.score');
+const resultsText = document.querySelector('.results');
+
+console.log(buttons);
+
+function updateScore(result, playerSelection, computerSelection) {
+
+    switch(result) {
+     case -1:
+        computerScore++;
+        resultsText.textContent = ("You lose! " + computerSelection + " beats " + playerSelection + ".");
+         break;
+    case 0:
+        resultsText.textContent = ("It's a tie!");
+        break;
+    case 1:
+        playerScore++;
+        resultsText.textContent = ("You win! " + playerSelection + " beats " + computerSelection + ".");
+    }
+
+    score.textContent = "Player : " + playerScore + " Computer: " + computerScore;
+
+    if (playerScore >= 5) {
+        resultsText.textContent = ("It's over! Player is first to get to 5!");
+    }
+    else if (computerScore >= 5) {
+        resultsText.textContent = ("OOF! CPU is first to get to 5!");
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = playerPlay();
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
         let computerSelection = computerPlay();
+        let result = playRound(button.id, computerSelection);
+        updateScore(result, button.id, computerSelection);
+    });
+});
 
-        result = playRound(playerSelection, computerSelection);
 
-        switch(result){
-            case -1:
-                computerScore++;
-                console.log("You lose! " + computerSelection + " beats " + playerSelection + ".");
-                break;
-            case 0:
-                console.log("It's a tie!");
-                break;
-            case 1:
-                playerScore++;
-                console.log("You win! " + playerSelection + " beats " + computerSelection + ".");
-        }
-    }
-
-    console.log("Final Score: Player - " + playerScore + ", Computer - " + computerScore);
-}
 
 //const playerSelection = playerPlay();
 //const computerSelection = computerPlay();
 
 //console.log(playRound(playerSelection, computerSelection));
 
-game();
+//game();
